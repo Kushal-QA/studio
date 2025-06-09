9f6f58012d2a6e0e769193617a33568e14ca1211
Kushal Prakasam <kushal@getrapl.com>
2025-06-09 05:11:53 +0000

    FIX/Update google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,26 +29,16 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {!!GA_TRACKING_ID && (
-          <>
-            <Script
-              strategy="afterInteractive"
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        {/* Google tag (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JKN19CM9J0"></Script>
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-JKN19CM9J0');`}
             />
-            <Script
-              id="google-analytics"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });`,
-              }}
-            />
-          </>
-        )}
       </head>
       <body className={`${poppins.variable} ${inter.variable} font-sans antialiased`} suppressHydrationWarning>
         {children}
